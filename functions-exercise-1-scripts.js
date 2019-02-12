7ebc67ad767ae152f10d336b779de4ca0c54f180
Derek <derekdenhartigh@gmail.com>
2019-02-11 22:05:56 -0500

    just getting started

---
+++ functions-exercise-1-scripts.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+FUNCTIONS EXERCISE 1
+Skills: Functions
+● Write a function named getAreaOfCircle. It takes a radius parameter. It calculates and
+returns the area of a circle with that radius.
+● Write a function named getCircumferenceOfCircle. It takes a radius parameter. It
+calculates and returns the circumference of a circle with that radius.
+● Write a function named getAreaOfSquare. It takes a side parameter. It calculates and
+returns the area of a square with that side length.
+● Write a function named getAreaOfTriangle. It takes two parameters: base and height.
+It calculates and returns the area of a triangle with that base and height.
+Call each of these functions and log the result to the console. (NOTE: None of these functions
+should use console.log within them. Instead, they must return the calculated value.)
+*/
+
+// parseInt(string, radix); - radix = 10 for decimal system
+
+function getAreaOfCircle(){
+    let radius = paseInt(prompt("What's your radius?"), 10);
+}
